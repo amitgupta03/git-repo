bfaed2de0f43c18aad36014b39d219bc0578ef68
amitgupta03 <amitgup333@gmail.com>
2020-05-27 12:51:08 +0530

    TODO with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+      <TodoList/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    TodoList
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,115 @@
+<template>
+<div>
+    <v-card max-width="700" class="mx-auto">
+      <v-toolbar color="deep-purple accent-4"  dark >
+        <v-toolbar-title>Todo List</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+    <v-spacer></v-spacer>
+     <v-text-field label="Enter Todo Item" filled v-model="newTodo" @keyup.enter="addTodo"></v-text-field>
+  
+      <v-list >
+        <v-list-item  v-for="(todo,index) in todosFiltered"  :key="todo.id" >
+            
+             <v-checkbox  color="primary" v-model="todo.completed"></v-checkbox>
+             <div v-if="!todo.editing" @dblclick="editTodo(todo)"  :class="{ completed : todo.completed }">{{todo.title}}</div>
+             <v-text-field v-else  v-model="todo.title" @keyup.enter="doneEdit(todo)"  @blur="doneEdit(todo)" ></v-text-field>
+          <v-list-item-icon>
+            <v-icon @click="removeTodo(index)">x</v-icon>
+          </v-list-item-icon> 
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+        <v-footer>
+    <v-spacer></v-spacer>
+     <div style="float:left">{{ remaining }} items left</div>
+  </v-footer>
+  <div>
+      <v-btn small   @click="filter='all'">All</v-btn>
+      <v-btn small  @click="filter='active'">Active</v-btn>
+      <v-btn small  @click="filter='completed'">completed</v-btn>
+
+  </div>
+   
+    </v-card>
+</div>
+</template>
+<script>
+
+  export default {
+    name: 'todo-list',
+
+    data () {
+        return{
+            newTodo: '',
+            idForTodo: 3,
+            beforeEditCache: '',
+             filter: 'all',
+            todos: [
+                {
+                'id': 1,
+                'title': 'first todo item',
+                'completed': false,
+                'editing': false,
+                },
+                {
+                'id': 2,
+                'title': 'second todo item',
+                'completed': false,
+                'editing': false,
+                }
+            ]
+        }
+    },
+    computed: {
+        remaining() {
+          return this.todos.filter(todo => !todo.completed).length
+        },
+        todosFiltered() {
+            if (this.filter == 'all') {
+                return this.todos
+            } else if (this.filter == 'active') {
+                return this.todos.filter(todo => !todo.completed)
+            } else if (this.filter == 'completed') {
+                return this.todos.filter(todo => todo.completed)
+            }
+            return this.todos
+         }
+    },
+    methods:{
+        addTodo() {
+            if (this.newTodo) {
+                this.todos.push({
+                    id: this.idForTodo,
+                    title: this.newTodo,
+                    completed: false,
+                    editing: false,
+                })
+                this.newTodo = ''
+                this.idForTodo++;
+            }
+        },
+        removeTodo(index) {
+             this.todos.splice(index, 1)
+         },
+        editTodo(todo) {
+            this.beforeEditCache = todo.title
+            todo.editing = true
+        },
+        doneEdit(todo) {
+            if (todo.title.trim() == '') {
+                todo.title = this.beforeEditCache
+            }
+            todo.editing = false
+        }
+        
+    }
+  }
+</script>
+<style>
+  .completed {
+    text-decoration: line-through;
+    color: grey;
+  }
+  
+</style>
